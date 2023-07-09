48288a9fdcb778c2cd49c617cd6423c636c551e9
csierra1 <80158950+csierra1@users.noreply.github.com>
2023-07-09 18:44:18 +0200

    Update streamlit_app.py
    Prepopulate the filter to get example

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,10 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 #Choose Fruit Column as Index
 my_fruit_list=my_fruit_list.set_index('Fruit')
 
-# Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+# Let's put a pick list here so they can pick the fruit they want to include - we want to filter the table based on their chose, pre- populate the list to give example
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+
+
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
